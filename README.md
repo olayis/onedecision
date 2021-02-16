f68b6f0d32a6bd67476bd48f26e18d1c92f9d4ef
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-02-16 23:45:39 +0100

    Production ready

---
+++ README.md
@@ -0,0 +1,125 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <img src="public/android-chrome-512x512.png" alt="Logo" width="80" height="80">
+  
+  <h3 align="center">OneDecision</h3>
+
+  <p align="center">
+    Are you stuck on a decision to make? Tell the computer your options and let it randomly select an option for you.
+    <br />
+    <a href="#table-of-contents"><strong>Explore the docs ↓</strong></a>
+    <br />
+    <br />
+    <a href="https://onedecision.herokuapp.com" target="_blank">View Demo</a>
+    ·
+    <a href="https://github.com/olayis/onedecision/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/olayis/onedecision/issues">Request Feature</a>
+  </p>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [About the Project](#about-the-project)
+  - [Built With](#built-with)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Roadmap](#roadmap)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+<img src="public/img/onedecision.png" alt="Screenshot" style="max-width: 100%; height: auto;">
+
+### Built With
+
+- Html
+- SCSS
+- React.js
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+- If you don't already have the following installed on your system, click on the links below to install:
+
+  - Node.js - <https://nodejs.org/en/>
+  - yarn - <https://classic.yarnpkg.com/en/docs/install/>
+
+### Installation
+
+1.Clone the onedecision repository
+
+    git clone https://github.com/olayis/onedecision.git
+
+2.Install all the dependencies of the project
+
+    yarn
+
+3.Run webpack development server
+
+    yarn build:dev
+
+    Runs the app in the development mode.
+
+    Open http://localhost:3000 to view it in the browser.
+
+    The page will reload if you make edits.
+
+4.Run webpack production build
+
+    yarn build:prod
+
+    Builds the app for production.
+
+5.Start up server to view app in production mode
+
+    yarn start
+
+    Open http://localhost:9000 to view the production mode in the browser.
+
+## Roadmap
+
+See the [open issues](https://github.com/olayis/onedecison/issues) for a list of proposed features (and known issues).
+
+<!-- CONTRIBUTING -->
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- CONTACT -->
+
+## Contact
+
+Olayinka Olusegun
+
+- olayinkasegunsolo@gmail.com
+- LinkedIn - <https://linkedin.com/in/olusegun-olayinka>
+- Twitter - [@olusegun_os](https://twitter.com/olusegun_os)
+
+Project Link: <https://onedecision.herokuapp.com>
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,115 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
+  mode: isDevelopment ? 'development' : 'production',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: isDevelopment ? 'bundle.js' : '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: !isDevelopment },
+          },
+        ],
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        port: 9000
-    }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|ico)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: !isDevelopment,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/favicon.png',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css',
+    }),
+  ],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.scss',
+      '.gif',
+      '.png',
+      '.jpg',
+      '.jpeg',
+      '.svg',
+    ],
+  },
+  devtool: isDevelopment ? 'inline-source-map' : 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+  },
+  optimization: {
+    minimize: isDevelopment ? false : true,
+    minimizer: isDevelopment ? [] : [new TerserPlugin()],
+  },
 };
